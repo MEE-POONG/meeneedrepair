f36ffe3a9688a04274389b59b8e80b70b0f5b6d4
Ploy <117616170+yaminP@users.noreply.github.com>
2023-08-13 16:55:48 +0700

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,22 @@
+import Link from "next/link";
+
 const Footer = () => {
     return (
-        <footer className="bg-[#F4F5F5]">
-            <div className="md:flex justify-center lg:justify-evenly">
-            
-                <div>
+        <div className="">
+        <footer className="bg-[#F4F5F5] py-10 px-2 ">
+            <div className="md:grid grid-cols-3 lg:grid-cols-5 justify-center container mx-auto gap-4   ">
                 <img
                     className="w-44"
                     src="./images/LOGO.png"
                     alt=""
                 />
-                    <p>Mee Need Repair</p>
+                <div >
+                    <p className="text-4xl font-extrabold text-transparent 
+                                      bg-gradient-to-r from-blue-600 to-sky-400
+                                      bg-clip-text
+                            ">
+                        Mee Need Repair
+                    </p>
                     <p>หน้าแรก</p>
                     <p>เกี่ยวกับเรา</p>
                     <p>ข่าวสาร</p>
@@ -17,23 +24,20 @@ const Footer = () => {
                 </div>
 
                 <div>
-                    <p>Mee Need Repair</p>
-                    <p>หน้าแรก</p>
-                    <p>เกี่ยวกับเรา</p>
-                    <p>ข่าวสาร</p>
-                    <p>ติดต่อเรา</p>
+                    <p className="text-2xl my-3">บริกรของเรา</p>
+                    <p>รับซ่อมบำรุงอุปกรณ์ IT</p>
+                    <p>ให้คำปรึกษาปัญหาเกี่ยวกับอุปกรณ์ IT</p>
                 </div>
 
                 <div>
-                    <p>Mee Need Repair</p>
-                    <p>หน้าแรก</p>
-                    <p>เกี่ยวกับเรา</p>
-                    <p>ข่าวสาร</p>
-                    <p>ติดต่อเรา</p>
+                    <p className="text-2xl my-3">สนับสนุน</p>
+                    <p>วิธีการสมัครสมาชิก</p>
+                    <p>วิธีการสั่งซื้อ และชำระเงิน</p>
+                    <p>การติดตามสถานะ</p>
                 </div>
 
                 <div>
-                    <p>Mee Need Repair</p>
+                    <p  className="text-2xl my-3">Mee Need Repair</p>
                     <p>หน้าแรก</p>
                     <p>เกี่ยวกับเรา</p>
                     <p>ข่าวสาร</p>
@@ -41,6 +45,12 @@ const Footer = () => {
                 </div>
             </div>
         </footer>
+
+        <div className="bg-[#0F172A] text-white text-center py-5"> 
+                <div >©2023 <Link href=''>Me Prompt technology Co.</Link>,Ltd. All Rights Reserved.</div>
+            </div>
+        
+        </div>
     )
 }
 export default Footer;
