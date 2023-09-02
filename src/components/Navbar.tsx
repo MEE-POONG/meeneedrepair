64a11fd40d2a9edb31394440353cd37714c9428c
Ploy <117616170+yaminP@users.noreply.github.com>
2023-09-02 13:58:05 +0700

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { HiBars3BottomLeft, HiOutlineXMark, HiUser } from "react-icons/hi2";
 
 
 const Navbar = () => {
-  const [scroll, setScroll] = useState(0);
 
+  const Links = [
+    { name: 'หน้าแรก', link: '/' },
+    { name: 'เกี่ยวกับเรา', link: 'about' },
+    { name: 'บริการของเรา', link: 'services' },
+    { name: 'สินค้า', link: 'product' },
+    { name: 'ติดต่อ', link: 'contact' },
+  ];
+
+  const [scroll, setScroll] = useState(0);
   useEffect(() => {
     const handleScroll = () => {
       setScroll(window.scrollY);
@@ -15,6 +24,8 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+
+
   const [isOpen, setIsOpen] = useState(false);
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -22,59 +33,90 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className="container mx-auto flex items-center justify-between md:rounded-[100px] md:px-5 md:py-2" style={{ backgroundColor: scroll > 300 ? "#F4F5F5" : "" }}>
-        <div className="flex">
-          <Image
-            src="/images/LOGO.png"
-            width={110}
-            height={110}
-            className=" " alt="" />
+      <div className="container mx-auto flex items-center justify-between md:rounded-[100px] md:px-5 md:py-2 " style={{ backgroundColor: scroll > 200 ? "#F4F5F5" : "" }}>
+        <div className="flex items-center">
+          <div onClick={() => setIsOpen(!isOpen)}
+            className="md:hidden w-10 h-10"
+            style={{ color: scroll > 200 ? "" : "#F4F5F5" }}>
+            {
+              isOpen ? <HiOutlineXMark size={40} /> : <HiBars3BottomLeft size={40} />
+            }
 
-          
-
+          </div>
+          <div className="flex">
+            <Image
+              src="/images/LOGO.png"
+              width={100}
+              height={100}
+              className="w-[90px]" alt="" />
+          </div>
         </div>
-        <button
-            onClick={toggleDropdown}
-            data-collapse-toggle="mobile-menu-3"
-            type="button"
-            className="md:hidden text-primary  focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex justify-between items-center "
-            aria-controls="mobile-menu-3"
-            aria-expanded={isOpen ? "true" : "false"}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
+
+        {/* nav link here */}
+        <ul className="md:flex gap-10 hidden font-semibold items-center" >
+          {Links.map((link) => (
+            <li key={link.name} style={{ color: scroll > 200 ? "" : "#F4F5F5" }}>
+              <a href={link.link} >{link.name}</a>
+            </li>
+          ))}
+
+        </ul>
+        {/* login && badket */}
+        <ul className="flex gap-3 font-semibold items-center">
+          <li className="flex items-center">
+            <HiUser size={20} style={{ color: scroll > 200 ? "" : "#F4F5F5" }} />
+            <span className="hidden">เข้าสู่ระบบ</span>
+          </li>
+          <div className="bg-natural04 w-[1px] h-10 "></div>
+
+          <li className="mr-5 ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 30 30" fill="none">
+              <path d="M8.5 18H6.5V26H8.5V18Z" fill="url(#paint0_linear_220_322)" />
+              <path d="M13.5 18H11.5V26H13.5V18Z" fill="url(#paint1_linear_220_322)" />
+              <path d="M18.5 18H16.5V26H18.5V18Z" fill="url(#paint2_linear_220_322)" />
+              <path d="M23.5 18H21.5V26H23.5V18Z" fill="url(#paint3_linear_220_322)" />
+              <path d="M28.6051 9.5L23.6289 0H21.3711L26.3473 9.5H3.65269L8.62887 0H6.37113L1.39494 9.5H0V16.4667L2.6785 30H27.3198L30 16.599V9.5H28.6051ZM28 16.401L25.6802 28H4.3215L2 16.2708V16H28V16.401ZM28 14H2V11.5H28V14Z" fill="url(#paint4_linear_220_322)" />
+              <defs>
+                <linearGradient id="paint0_linear_220_322" x1="6.5" y1="21.9835" x2="8.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#CA0808" />
+                  <stop offset="1" stop-color="#0FC0E7" />
+                </linearGradient>
+                <linearGradient id="paint1_linear_220_322" x1="11.5" y1="21.9835" x2="13.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#CA0808" />
+                  <stop offset="1" stop-color="#0FC0E7" />
+                </linearGradient>
+                <linearGradient id="paint2_linear_220_322" x1="16.5" y1="21.9835" x2="18.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#CA0808" />
+                  <stop offset="1" stop-color="#0FC0E7" />
+                </linearGradient>
+                <linearGradient id="paint3_linear_220_322" x1="21.5" y1="21.9835" x2="23.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#CA0808" />
+                  <stop offset="1" stop-color="#0FC0E7" />
+                </linearGradient>
+                <linearGradient id="paint4_linear_220_322" x1="-2.48931e-07" y1="14.9383" x2="27.963" y2="14.9383" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#CA0808" />
+                  <stop offset="1" stop-color="#0FC0E7" />
+                </linearGradient>
+              </defs>
             </svg>
-            <svg
-              className="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        <ul className="md:flex gap-5 hidden">
-          <Link href="./"
-            style={{ color: scroll > 300 ? "#333" : "" }}
-            className=" text-white hover:bg-[#18BCEB] hover:text-white px-3 py-2 rounded-full text-20 font-medium "
-          >  หน้าแรก
-          </Link>
+          </li>
+        </ul>
+
+
+      </div>
+      {/* nav link for mobile here */}
+      <div className={`p-5 absolute z-[-1] w-full backdrop-blur-xl bg-white/10 transition-all ease-in ${isOpen ? '':'hidden'}` }
+        style={{ backgroundColor: scroll > 200 ? "#F4F5F5" : "" }}>
+        <ul className="font-semibold items-center" >
+          {Links.map((link) => (
+            <li key={link.name} style={{ color: scroll > 200 ? "" : "#F4F5F5" }}>
+              <a href={link.link} >{link.name}</a>
+            </li>
+            // 
+          ))}
         </ul>
       </div>
+
     </nav>
   );
 };
