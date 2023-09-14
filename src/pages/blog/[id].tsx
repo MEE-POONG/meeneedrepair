c0c5a25b23be550527d89eb16553f34009db43a8
wechanon <wechanon2001@gmail.com>
2023-09-14 10:47:06 +0700

    blog

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import RootLayout from "../../components/layout";
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import LatestBlog from "../../container/Blog/Latestblog";
 
 const ReadBlogDetail = () => {
     const router = useRouter();
@@ -84,37 +85,15 @@ const ReadBlogDetail = () => {
                         </div>
 
                         {/* Right Content */}
-                        <div className="col-span-2 bg-[#1E293B] mt-10 rounded-lg p-6">
-                            <p className="text-white text-xl">ข่าวแนะนำ</p>
-                            <div className="bg-[#F4F5F5] rounded-[20px] mx-auto">
-                                <div className="p-3 ">
-                                    <img className="rounded-[20px] w-full "
-                                        src="https://res.cloudinary.com/practicaldev/image/fetch/s--99fg18pd--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/mtjqqj22x8g1nwubmb1c.png"
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="px-4 py-2">
-                                    <h4 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#081FF0]  to-[#13D1D1] ">หัวข้อข่าว</h4>
-                                    <div className="flex justify-between px-2 text-sm">
-                                        <p>By ที่มาของข่าว</p>
-                                        <p>2 ส.ค. 66</p>
-                                    </div>
-                                    <p className="mt-2 truncate">อธิบายรายละเอียด อธิบายรายละเอียดอธิบายรายละเอียดอธิบายรายละเอียดอธิบายรายละเอียดอธิบายรายละเอียดอธิบายรายละเอียดอธิบายรายละเอียด</p>
-                                    <Link href='blog/readBlog'>
-                                        <p className="bg-yellow-500 p-2 rounded-full text-center mt-3 ">
-                                            อ่าน
-                                        </p>
-                                    </Link>
-                                </div>
+                       
 
-
-                            </div>
+                            <LatestBlog/>
 
                         </div>
                     </div>
                 </div>
 
-            </div>
+     
         </RootLayout>
     )
 }
