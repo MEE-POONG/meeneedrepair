0f07e9cc3435cda0a67124f733f4188cb6b4cc99
Ploy <117616170+yaminP@users.noreply.github.com>
2023-09-20 14:51:06 +0700

    poi

---
+++ src/pages/howto_register.tsx
@@ -0,0 +1,14 @@
+import RootLayout from "../components/layout";
+
+export default function HowtoRegister(){
+    return(
+        <>
+        <title>วิธีการสมัครสมาชิก</title>
+        <RootLayout>
+            <div className="container mx-auto my-24 bg-secondary2 rounded-lg p-10">
+                <h2>วิธีการสมัครสมาชิก</h2>
+            </div>
+        </RootLayout>
+        </>
+    )
+}
