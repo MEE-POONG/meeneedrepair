b007550432c301c934c5928d6e8d6ae9a6956d13
ken1lnw <moowinza@gmail.com>
2023-09-22 11:15:35 +0700

    เพิ่ม Modal รายการสั่งซื้อ และ Modal รายการซ่อม

--- src/container/Profile/order_profile.tsx
+++ src/container/Profile/order_profile.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ModalOrder from './ModalOrder';
 
 const OrderProfile = () => {
     return (
@@ -22,13 +23,7 @@ const OrderProfile = () => {
                     </div>
 
                     <div className="flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-file-description" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                            <path d="M9 17h6"></path>
-                            <path d="M9 13h6"></path>
-                        </svg>
+                        <ModalOrder />
                     </div>
 
                 </div>
@@ -43,13 +38,7 @@ const OrderProfile = () => {
                     </div>
 
                     <div className="flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-file-description" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                            <path d="M9 17h6"></path>
-                            <path d="M9 13h6"></path>
-                        </svg>
+                        <ModalOrder />
                     </div>
 
                 </div>
@@ -63,13 +52,7 @@ const OrderProfile = () => {
                     </div>
 
                     <div className="flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-file-description" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                            <path d="M9 17h6"></path>
-                            <path d="M9 13h6"></path>
-                        </svg>
+                        <ModalOrder />
                     </div>
 
                 </div>
@@ -84,13 +67,7 @@ const OrderProfile = () => {
                     </div>
 
                     <div className="flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-file-description" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-                            <path d="M9 17h6"></path>
-                            <path d="M9 13h6"></path>
-                        </svg>
+                        <ModalOrder />
                     </div>
 
                 </div>
