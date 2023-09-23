158415148ecf189c495ed9805841684ae7ee0bfc
wechanon <wechanon2001@gmail.com>
2023-09-23 14:13:50 +0700

    product

--- src/components/shoppingCart/ShopCart.tsx
+++ src/components/shoppingCart/ShopCart.tsx
@@ -3,6 +3,7 @@ import RootLayout from "../layout";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import Link from "next/link";
 import { useState } from "react";
+import Product1 from "../../pages/producttest";
 
 
 
@@ -25,7 +26,8 @@ const ShoppingCart = () => {
       <h3 className="text-lg md:text-2xl text-secondary2">รายการสินค้า <span className="">(2)</span></h3>
       <div className="grid grid-flow-row lg:grid-cols-12 gap-2 lg:gap-8 mt-3 lg:mt-8 ">
         <div className="lg:col-span-9 bg-secondary1 rounded-md">
-          <div className="p-2 md:p-9">
+        <Product1/>
+          {/* <div className="p-2 md:p-9">
             <div className="flex justify-between items-center text-xs md:text-sm">
               <Image
                 src="/images/joystick.png"
@@ -41,7 +43,7 @@ const ShoppingCart = () => {
               <div className="flex items-center">
                 <span onClick={decreaseQuantity} className="cursor-pointer text-secondary2"> - </span>
       <p className="h-5 w-8 rounded-full border bg-white text-center outline-none mx-2">{quantity}</p>
-                {/* <input className="h-5 w-8 rounded-full border bg-white text-center outline-none mx-2 " type="number" defaultValue="1" min="1" /> */}
+                <input className="h-5 w-8 rounded-full border bg-white text-center outline-none mx-2 " type="number" defaultValue="1" min="1" />
                 <span onClick={increaseQuantity} className="cursor-pointer  text-secondary2"> + </span>
               </div>
 
@@ -51,7 +53,6 @@ const ShoppingCart = () => {
             </div>
             <hr />
           </div>
-
           <div className="p-2 md:p-9">
             <div className="flex justify-between items-center text-xs md:text-sm">
               <Image
@@ -68,7 +69,7 @@ const ShoppingCart = () => {
               <div className="flex items-center">
               <span onClick={decreaseQuantity} className="cursor-pointer text-secondary2"> - </span>
       <p className="h-5 w-8 rounded-full border bg-white text-center outline-none mx-2">{quantity}</p>
-                {/* <input className="h-5 w-8 rounded-full border bg-white text-center outline-none mx-2 " type="number" defaultValue="1" min="1" /> */}
+                <input className="h-5 w-8 rounded-full border bg-white text-center outline-none mx-2 " type="number" defaultValue="1" min="1" />
                 <span onClick={increaseQuantity} className="cursor-pointer  text-secondary2"> + </span>
               </div>
 
@@ -77,8 +78,9 @@ const ShoppingCart = () => {
               </button>
             </div>
             <hr />
-          </div>
+          </div> */}
         </div>
+        
 
         <div className="lg:col-span-3 md:h-48 bg-secondary2 rounded-md p-2 lg:p-5 text-center">
           <div className="text-left text-xs md:text-sm">
