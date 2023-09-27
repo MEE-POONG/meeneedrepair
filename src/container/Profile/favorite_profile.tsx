2c7fbe9a2c82cb63226e17603346895833c42d16
ken1lnw <moowinza@gmail.com>
2023-09-27 11:29:51 +0700

    เพิ่ม api productsitem สำหรับ รายการสินค้า

--- src/container/Profile/favorite_profile.tsx
+++ src/container/Profile/favorite_profile.tsx
@@ -26,32 +26,7 @@ const FavoriteProfile = () => {
                 </div>
 
 
-                <div className="bg-secondary2 rounded-2xl p-3 grid grid-cols-12 mb-5">
-                    <img src="/images/joystick.png" alt="" className="col-span-2 md:col-span-2 h-24 w-24 object-contain" />
-                    <p className="col-span-4 md:col-span-6 self-center truncate">ชื่อสินค้า ถ้ายาวจะเป็นแบบนี้ อันนี้คือความยาวของตัวหนังสือ ถ้ายาว </p>
-                    <p className="col-span-2 md:col-span-1 self-center text-[#FF5C74] ">฿ 490</p>
-                    <button className="col-span-3 md:col-span-2 bg-[#FFCD4B] rounded-xl h-14 md:h-10 self-center">หยิบใส่ตระกร้า</button>
-                    <div className="flex items-center justify-center">
-                        <svg className="self-center" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 32 32">
-                            <path d="M 15 4 C 14.476563 4 13.941406 4.183594 13.5625 4.5625 C 13.183594 4.941406 13 5.476563 13 6 L 13 7 L 7 7 L 7 9 L 8 9 L 8 25 C 8 26.644531 9.355469 28 11 28 L 23 28 C 24.644531 28 26 26.644531 26 25 L 26 9 L 27 9 L 27 7 L 21 7 L 21 6 C 21 5.476563 20.816406 4.941406 20.4375 4.5625 C 20.058594 4.183594 19.523438 4 19 4 Z M 15 6 L 19 6 L 19 7 L 15 7 Z M 10 9 L 24 9 L 24 25 C 24 25.554688 23.554688 26 23 26 L 11 26 C 10.445313 26 10 25.554688 10 25 Z M 12 12 L 12 23 L 14 23 L 14 12 Z M 16 12 L 16 23 L 18 23 L 18 12 Z M 20 12 L 20 23 L 22 23 L 22 12 Z"></path>
-                        </svg>
-                    </div>
 
-                </div>
-
-                <div className="bg-secondary2 rounded-2xl p-3 grid grid-cols-12 mb-5">
-                    <img src="/images/joystick.png" alt="" className="col-span-2 md:col-span-2 h-24 w-24 object-contain" />
-                    <p className="col-span-4 md:col-span-6 self-center truncate">ชื่อสินค้า</p>
-                    <p className="col-span-2 md:col-span-1 self-center text-[#FF5C74] ">฿ 490</p>
-                    <button className="col-span-3 md:col-span-2 bg-[#FFCD4B] rounded-xl h-14 md:h-10 self-center">หยิบใส่ตระกร้า</button>
-                    <div className="flex items-center justify-center">
-                        <svg className="self-center" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 32 32">
-                            <path d="M 15 4 C 14.476563 4 13.941406 4.183594 13.5625 4.5625 C 13.183594 4.941406 13 5.476563 13 6 L 13 7 L 7 7 L 7 9 L 8 9 L 8 25 C 8 26.644531 9.355469 28 11 28 L 23 28 C 24.644531 28 26 26.644531 26 25 L 26 9 L 27 9 L 27 7 L 21 7 L 21 6 C 21 5.476563 20.816406 4.941406 20.4375 4.5625 C 20.058594 4.183594 19.523438 4 19 4 Z M 15 6 L 19 6 L 19 7 L 15 7 Z M 10 9 L 24 9 L 24 25 C 24 25.554688 23.554688 26 23 26 L 11 26 C 10.445313 26 10 25.554688 10 25 Z M 12 12 L 12 23 L 14 23 L 14 12 Z M 16 12 L 16 23 L 18 23 L 18 12 Z M 20 12 L 20 23 L 22 23 L 22 12 Z"></path>
-                        </svg>
-                    </div>
-
-                </div>
-                
 
 
 
