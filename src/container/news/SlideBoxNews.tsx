5f40d433f6ccd87b177ceffc49588e461f267b88
l3enzlz <150216191+l3enzlz@users.noreply.github.com>
2023-11-25 09:36:49 +0700

    News

--- src/container/news/SlideBoxNews.tsx
+++ src/container/news/SlideBoxNews.tsx
@@ -1,23 +1,187 @@
 
 export default function SlideBoxNews() {
     return (
-        <div className="grid grid-cols-12">
-            <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
-                <img src="images/blog.jpg" alt="img-blur-shadow" />
-                <div className="mt-2">
-                    <h5 className="block">UI/UX Review Check</h5>
-                </div>
-                <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
 
-
-                <div className="col-span-2">
-                    <img src="images/blog.jpg" alt="img-blur-shadow" />
-                    <div className="mt-2">
-                        <h5 className="block">UI/UX Review Check</h5>
+        <div className="container mx-auto  ">
+            <div className="grid grid-cols-12 ">
+                <div className="col-span-2 my-5">
+                    <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                        <a href="/news/readNews">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </a>
                     </div>
-                    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
                 </div>
-            </div >
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="col-span-2 my-5">
+                    <a href="/news/readNews">
+                        <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+                            <img src="images/blog.jpg" alt="img-blur-shadow" />
+                            <div className="mt-2">
+                                <h5 className="block">UI/UX Review Check</h5>
+                            </div>
+                            <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+
+
+
         </div>
     )
 }
+
+// ใส่เป็น แนะนำข่าว,บทความ
+// <div className="container mx-auto">
+// <div className="grid grid-cols-12 ">
+//     <div className="relative   text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+//         <div className="col-span-2">
+//             <div>
+//                 <img src="images/blog.jpg" alt="img-blur-shadow" />
+//             </div>
+//             <div className="mt-2">
+//                 <h5 className="block">UI/UX Review Check</h5>
+//             </div>
+//             <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+
+//         </div>
+//     </div>
+// </div>
+// </div>
+
+
+
+
+
+
+
+
+
+// export default function SlideBoxNews() {
+//     return (
+//         <div className="container mx-auto">
+//             <div className="grid grid-cols-12 ">
+//                 <div className="  text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+//                     <div className="col-span-4">
+//                         <img src="images/blog.jpg" alt="img-blur-shadow" />
+//                         <div className="mt-2">
+//                             <h5 className="block">UI/UX Review Check</h5>
+//                         </div>
+//                         <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+//                     </div>
+
+//                     <div className="col-span-3">
+//                         <img src="images/blog.jpg" alt="img-blur-shadow" />
+//                         <div className="mt-2">
+//                             <h5 className="block">UI/UX Review Check</h5>
+//                         </div>
+//                         <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+//                     </div>
+
+
+//                 </div>
+//             </div>
+//         </div>
+
+//         <div className="container">
+//             <div className="grid grid-cols-12">
+//                 <div className="col-span-3">
+//                 <img src="images/blog.jpg" alt="img-blur-shadow" />
+//                         <div className="mt-2">
+//                             <h5 className="block">UI/UX Review Check</h5>
+//                         </div>
+//                         <p className="block font-sans text-[14px] antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// // ใส่เป็น แนะนำข่าว,บทความ
+// // <div className="container mx-auto">
+// // <div className="grid grid-cols-12 ">
+// //     <div className="relative   text-gray-700 bg-white shadow-md w-60 rounded-xl bg-clip-border">
+// //         <div className="col-span-3">
+// //             <div>
+// //                 <img src="images/blog.jpg" alt="img-blur-shadow" />
+// //             </div>
+// //             <div className="mt-2">
+// //                 <h5 className="block">UI/UX Review Check</h5>
+// //             </div>
+// //             <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
+
+// //         </div>
+// //     </div>
+// // </div>
+// // </div>
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import RootLayout from "../components/layout";
-import StepperForm from '../container/Stepper/StepperForm';
 
 export default function Payment() {
     return (
         <RootLayout>
             <div className="container mx-auto">
-                <StepperForm/>
+              
             </div>
         </RootLayout>
     )
