80bb9dc12d1e6d5ca869eaaa7af1c19f85b9f164
l3enzlz <150216191+l3enzlz@users.noreply.github.com>
2023-12-06 09:42:49 +0700

    จองงๆๆ

--- src/pages/appointment/[id].tsx
+++ src/pages/appointment/[id].tsx
@@ -203,7 +203,7 @@ const UpdateUserData = () => {
         }
 
     }
-}
+}//
     return (
         <RootLayout>
       
@@ -327,6 +327,7 @@ const UpdateUserData = () => {
                             </div>
                         </div>
                     )}
+                    
                     {isModalOpen && (
                         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
                             <div className="bg-white p-6 rounded-lg">
